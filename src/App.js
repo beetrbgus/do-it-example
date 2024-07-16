dc6ad4c152d5efb598ea99183cb129e7e699f753
beetrbgus <devbeetrb0214@gmail.com>
2024-07-16 11:30:34 +0900

    feat - 다양한 프로퍼티 값 사용하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropsComponent from './03/PropsComponent';
+import ChildComponent from './03/ChildComponent';
 
 class App extends React.Component {
   render() {
     return (
       <div className="body">
-        <PropsComponent name="message"/>
+        <ChildComponent
+          boolValue= {true}
+          numValue= {1}
+          arrayValue= {[1, 2, 3]}
+          objValue= {JSON.stringify({name : "제목", age : 30})}
+          nodeValue= {<h1>노드</h1>}
+          funcValue= {()=> {console.log('메시지')}}
+        />
       </div>
     );
   }
