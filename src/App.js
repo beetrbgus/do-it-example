41912ef4b6f8961b5355f80ec6adafea319d9aa7
beetrbgus <devbeetrb0214@gmail.com>
2024-07-16 15:19:11 +0900

    feat - 프로퍼티 children 전달

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import PropsComponent from './03/PropsComponent';
 import ChildComponent from './03/ChildComponent';
 import ChildComponent2 from './03/ChildComponent2';
 import DefaultPropsComponent from './03/DefaultPropsComponent';
+import ChildProperty from './03/ChildProperty';
 
 class App extends React.Component {
   render() {
     return (
       <div className="body">
-        <DefaultPropsComponent/>
+        <ChildProperty>
+          <div><span>자식 노드</span></div>
+        </ChildProperty>
       </div> 
     );
   }
