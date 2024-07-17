d791707f4f891c76c9c5922355ca24d3e0f5d37d
beetrbgus <devbeetrb0214@gmail.com>
2024-07-17 10:43:42 +0900

    feat - 버튼 누르면 숫자 올라가는 state 기능 실습

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import DefaultPropsComponent from './03/DefaultPropsComponent';
 import ChildProperty from './03/ChildProperty';
 import StateExam from './03/StateExam';
 import ForceUpdateExample from './03/ForceUpdateExample';
+import Count from './03/Count';
 
 class App extends React.Component {
   render() {
     return (
       <div className="body">
-        <ForceUpdateExample/>
+        <Count/>
       </div> 
     );
   }
